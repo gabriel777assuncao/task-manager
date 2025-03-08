422d07c968158dab1498f5593f382a0fd4164b84
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-03-07 21:00:51 -0300

    feat: installing phpcs and ecs for coding style.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,15 +9,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory;
-    use Notifiable;
+    use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
